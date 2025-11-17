01c1784b9aff0666c9d6fed7bb9cb9aacec65819
Tasnimul Hassan <tasnimulhassan02@gmail.com>
2025-11-17 20:16:11 +0600

    Updated frontend

--- index.js
+++ index.js
@@ -6,25 +6,23 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-
 app.get("/", (req,res) => {
     res.render("index.ejs");
 })
 
-
-app.post("/submit", async (req, res) => {
-try {
-    const response = await axios.get("https://api.animechan.io/v1/quotes/random");
-    const result = response.data;
-    console.log(result);
-    res.render("index.ejs", { content: result });
-  } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: error.message,
-    });
-  }
-});
+// app.get("/", async (req, res) => {
+// try {
+//     const response = await axios.get("https://api.animechan.io/v1/quotes/random");
+//     const result = response.data;
+//     console.log(result);
+//     res.render("index.ejs", { content: result });
+//   } catch (error) {
+//     console.error("Failed to make request:", error.message);
+//     res.render("index.ejs", {
+//       error: error.message,
+//     });
+//   }
+// });
 
 
 
